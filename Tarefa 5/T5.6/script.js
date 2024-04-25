39aeab0b1dfe26bd16f7f764b6ba18357d06d162
Regisson Andre de Lara Filho <99519216+RegissonAF@users.noreply.github.com>
2024-04-25 20:39:17 -0300

    Separando os scripts em files diferentes e fazendo o exercicio 6.

---
+++ Tarefa 5/T5.6/script.js
@@ -0,0 +1,9 @@
+let array = [1,2,3,4,5,6,7,8,9,10];
+let finalarray = [];
+
+    array.forEach(element => {
+        if(element%2 == 0){
+            finalarray.push(element);
+        }}
+    )
+    console.log(finalarray);
